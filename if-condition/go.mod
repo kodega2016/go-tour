5435c44761fb83a739b5166c6cf138d7329c6b8d
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2024-10-24 02:54:06 +0545

    explore if statement in golang

---
+++ if-condition/go.mod
@@ -0,0 +1,3 @@
+module if-condition
+
+go 1.23.1
---
+++ if-condition/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 10
+	if age < 18 {
+		fmt.Println("You are under 18")
+	} else {
+		fmt.Println("You are welcome to this application")
+	}
+}
